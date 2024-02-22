b8ff441094952cd18f1b37e1c14190071e2406d7
ebagabe <gabriel.ebagabe@gmail.com>
2024-02-21 23:33:01 -0300

    Projeto Finalizando

--- script.js
+++ script.js
@@ -39,6 +39,10 @@ const handlePressButton = () => {
     const month = Number(document.querySelector('#month').value);
     const year = Number(document.querySelector('#year').value);
 
+    const dayInput = document.querySelector('#day');
+    const monthInput = document.querySelector('#month');
+    const yearInput = document.querySelector('#year');
+
     const errorContainer = document.querySelector('#errorContainer');
     errorContainer.innerHTML = '';
 
@@ -46,45 +50,72 @@ const handlePressButton = () => {
 
     if (isNaN(day) || isNaN(month) || isNaN(year)) {
         displayError("Utilize numeros válidos para Dia, Mês ou Ano");
-        
-        dayResult.textContent = "--";
-        monthResult.textContent = "--";
-        yearResult.textContent = "--";
-        
-        return;
-    }
     
-    if (day < 1 || day > maxDaysInMonth) {
-        displayError(`O dia deve ser entre 1 e ${maxDaysInMonth}`);
-        
         dayResult.textContent = "--";
         monthResult.textContent = "--";
         yearResult.textContent = "--";
-        
+    
+        dayInput.style.border = "1px solid red";
+        monthInput.style.border = "1px solid red";
+        yearInput.style.border = "1px solid red";
+    
         return;
     }
 
-    if (month < 1 || month > 12) {
-        displayError("O mês deve ser maior que 1 e menor que 12")
-        
+    dayInput.style.border = "1px solid rgba(128, 128, 128, 0.301)"
+    monthInput.style.border = "1px solid rgba(128, 128, 128, 0.301)"
+    yearInput.style.border = "1px solid rgba(128, 128, 128, 0.301)"
+
+
+    if (day < 1 || day > maxDaysInMonth) {
+        displayError(`O dia deve ser entre 1 e ${maxDaysInMonth}`);
+
         dayResult.textContent = "--";
         monthResult.textContent = "--";
         yearResult.textContent = "--";
-        
+
+        dayInput.style.border = "1px solid red";
+
         return;
     }
+
+    dayInput.style.border = "1px solid rgba(128, 128, 128, 0.301)"
+    monthInput.style.border = "1px solid rgba(128, 128, 128, 0.301)"
+    yearInput.style.border = "1px solid rgba(128, 128, 128, 0.301)"
+
+    if (month < 1 || month > 12) {
+        displayError("O mês deve ser maior que 1 e menor que 12")
+
+        dayResult.textContent = "--";
+        monthResult.textContent = "--";
+        yearResult.textContent = "--";
+
+        monthInput.style.border = "1px solid red";
+
+        return;
+    }
+
+    dayInput.style.border = "1px solid rgba(128, 128, 128, 0.301)"
+    monthInput.style.border = "1px solid rgba(128, 128, 128, 0.301)"
+    yearInput.style.border = "1px solid rgba(128, 128, 128, 0.301)"
 
     const birthday = new Date(year, month - 1, day);
     const currentDate = new Date();
 
-    if(birthday > currentDate) {
+    if (birthday > currentDate) {
         displayError("Não é permitido utilizar datas futuras");
 
         dayResult.textContent = "--";
         monthResult.textContent = "--";
         yearResult.textContent = "--";
+
+        yearInput.style.border = "1px solid red";
         return;
     }
+
+    dayInput.style.border = "1px solid rgba(128, 128, 128, 0.301)"
+    monthInput.style.border = "1px solid rgba(128, 128, 128, 0.301)"
+    yearInput.style.border = "1px solid rgba(128, 128, 128, 0.301)"
 
     const age = calculateAge(birthday);
 
