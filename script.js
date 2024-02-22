854dee37ee6fe859597da759cbeed95d03225e9c
ebagabe <gabriel.ebagabe@gmail.com>
2024-02-21 22:13:06 -0300

    DOM to birthday
    Co-authored-by: Joyce Kepler <joykepler@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const calculateAge = () => {
+const yearResult = document.querySelector('#yearResult');
+const monthResult = document.querySelector('#monthResult');
+const dayResult = document.querySelector('#dayResult');
+
+const calculateAge = (birthday) => {
     const currentDate = new Date();
     const currentYear = currentDate.getFullYear();
     const currentMonth = currentDate.getMonth() + 1;
@@ -8,16 +12,43 @@ const calculateAge = () => {
     const monthOfBirth = birthday.getMonth() + 1;
     const dayOfBirth = birthday.getDate();
 
-    let age = currentYear - yearOfBirth;
+    let ageInYears = currentYear - yearOfBirth;
+    let ageInMonths = currentMonth - monthOfBirth;
+    let ageInDays = currentDay - dayOfBirth;
 
-    if (currentMonth < monthOfBirth || (currentMonth === monthOfBirth && currentDay < dayOfBirth)) {
-        age--;
+    if (ageInDays < 0) {
+        const daysInLastMonth = new Date(currentYear, currentMonth - 1, 0).getDate();
+        ageInDays += daysInLastMonth;
+        ageInMonths--;
     }
 
-    // console.log(currentYear, currentMonth, currentDay, yearOfBirth, monthOfBirth, dayOfBirth);
-   console.log(age);
+    if (ageInMonths < 0) {
+        ageInMonths += 12;
+        ageInYears--;
+    }
+
+    return {
+        years: ageInYears,
+        month: ageInMonths,
+        days: ageInDays
+    }
 }
 
-const birthday = new Date(2005, 9, 15);
+const handleKeyPress = (event) => {
+    if (event.key === 't') {
+        const day = Number(document.querySelector('#day').value);
+        const month = Number(document.querySelector('#month').value);
+        const year = Number(document.querySelector('#year').value);
 
-calculateAge();
+        const birthday = new Date(year, month - 1, day);
+
+        const age = calculateAge(birthday);
+
+        dayResult.textContent = `${age.days}`;
+        monthResult.textContent = `${age.month}`;
+        yearResult.textContent = `${age.years}`;
+
+    }
+}
+
+document.addEventListener('keydown', handleKeyPress);
