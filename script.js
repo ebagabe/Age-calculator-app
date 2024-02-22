fc284090fcaa978def51e25ac8e77f45b84c4a72
ebagabe <gabriel.ebagabe@gmail.com>
2024-02-21 23:00:50 -0300

    Acrescentando validações

--- script.js
+++ script.js
@@ -34,21 +34,71 @@ const calculateAge = (birthday) => {
     }
 }
 
-const handleKeyPress = (event) => {
-    if (event.key === 't') {
-        const day = Number(document.querySelector('#day').value);
-        const month = Number(document.querySelector('#month').value);
-        const year = Number(document.querySelector('#year').value);
+const handlePressButton = () => {
+    const day = Number(document.querySelector('#day').value);
+    const month = Number(document.querySelector('#month').value);
+    const year = Number(document.querySelector('#year').value);
 
-        const birthday = new Date(year, month - 1, day);
+    const errorContainer = document.querySelector('#errorContainer');
+    errorContainer.innerHTML = '';
 
-        const age = calculateAge(birthday);
+    const maxDaysInMonth = new Date(year, month, 0).getDate();
 
-        dayResult.textContent = `${age.days}`;
-        monthResult.textContent = `${age.month}`;
-        yearResult.textContent = `${age.years}`;
-
+    if (isNaN(day) || isNaN(month) || isNaN(year)) {
+        displayError("Utilize numeros válidos para Dia, Mês ou Ano");
+        
+        dayResult.textContent = "--";
+        monthResult.textContent = "--";
+        yearResult.textContent = "--";
+        
+        return;
     }
+    
+    if (day < 1 || day > maxDaysInMonth) {
+        displayError(`O dia deve ser entre 1 e ${maxDaysInMonth}`);
+        
+        dayResult.textContent = "--";
+        monthResult.textContent = "--";
+        yearResult.textContent = "--";
+        
+        return;
+    }
+
+    if (month < 1 || month > 12) {
+        displayError("O mês deve ser maior que 1 e menor que 12")
+        
+        dayResult.textContent = "--";
+        monthResult.textContent = "--";
+        yearResult.textContent = "--";
+        
+        return;
+    }
+
+    const birthday = new Date(year, month - 1, day);
+    const currentDate = new Date();
+
+    if(birthday > currentDate) {
+        displayError("Não é permitido utilizar datas futuras");
+
+        dayResult.textContent = "--";
+        monthResult.textContent = "--";
+        yearResult.textContent = "--";
+        return;
+    }
+
+    const age = calculateAge(birthday);
+
+    dayResult.textContent = `${age.days}`;
+    monthResult.textContent = `${age.month}`;
+    yearResult.textContent = `${age.years}`;
+
 }
 
-document.addEventListener('keydown', handleKeyPress);
+const displayError = (errorMessage) => {
+    const errorContainer = document.querySelector('#errorContainer');
+    errorContainer.innerHTML = errorMessage;
+}
+
+const button = document.querySelector('#button')
+
+button.addEventListener('click', handlePressButton);
